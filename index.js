3e5abbea9d19c1d91dd678db21aaf8acac4d84fb
bill <bill.arnold@student.moringaschool.com>
2023-10-15 20:37:16 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code here!
+// index.js
+
+// Remove the existing 'main' element
+const mainElement = document.getElementById('main');
+mainElement.remove();
+
+// Create a new 'h1' element
+const newHeader = document.createElement('h1');
+
+// Set the id attribute to 'victory'
+newHeader.id = 'victory';
+
+// Set the innerHTML with the desired message
+newHeader.innerHTML = "YOUR-NAME is the champion";
+
+// Append the new 'h1' element to the body
+document.body.appendChild(newHeader);
